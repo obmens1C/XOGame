2292bd216e92dabaad78cce7075261ba51c41019
obmens1C <obmens1C@mail.ru>
2019-02-13 10:04:11 +0300

    Start

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,25 @@
 
 public class Player {
+    private String name;
+    private int numberOfWins;
 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfWins() {
+        return numberOfWins;
+    }
+
+    public void setNumberOfWins(int numberOfWins) {
+        this.numberOfWins = numberOfWins;
+    }
 }
---
+++ src/test/java/TestPlayer.java
@@ -0,0 +1,6 @@
+
+public class TestPlayer {
+    public static void main(String[] args) {
+        Player player1 = new Player("Den");
+    }
+}
